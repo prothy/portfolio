b591cb07bb17a3aed6673c831da97dd48154bce5
prothy <lukacsurgay@gmail.com>
2020-09-04 15:33:04 +0200

    js test

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<?php require '../lib/tmpl/header.php' ?>
-	<?php require '../lib/tmpl/load-script.php' ?>
+	<?php require '../lib/header.php' ?>
+	<?php require '../lib/load-script.php' ?>
 </body>
 </html>
