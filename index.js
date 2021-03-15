dc4fde8c078628ed4ce290aa29a578b31112be0a
prothy <lukacsurgay@gmail.com>
2021-03-15 13:53:04 +0100

    prepared site for language support, now need to turn object into html

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let lang = "en";
+let langlist = [];
+
+/* EXPRESS INIT */
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/assets", express.static(__dirname + "/public/assets"));
+
+/* LOCALIZATION PARSING */
 
 fs.readFile("./loc.json", "utf8", (err, jstring) => {
-  if (err) {
-    console.log("Something went wrong reading loc file: ", err);
-    return;
-  }
-
   try {
-    const artText = JSON.parse(jstring);
+    const textContent = JSON.parse(jstring);
     console.log(jstring);
-    // for (const element of Object.values(artText.en)) {
-    //   console.log(element[0]);
-    // }
+    console.log(Object.keys(textContent));
+    langlist = Object.keys(textContent);
+    // readlang(textContent[lang]);
   } catch (err) {
     console.log("Error parsing JSON string: ", err);
   }
 });
+
+// const readlang = (obj) => {
+//   let art = document.getElementsByClassName("main")[0];
+//   for (let key in obj) {
+//     console.log(obj[key]);
+//   }
+// };
+
+/* ROUTER HANDLING */
+
+app.get("", (req, res) => {
+  res.render("index");
+  lang = "en";
+});
+
+app.get("/:lang", (req, res) => {
+  if (!langlist.includes(req.params.lang)) {
+    res.redirect("/");
+    return;
+  }
+  lang = req.params.lang;
+  res.render("index");
+});
+
+/* HTML MODIFICATION */
+
+// const main = document.getElementsByClassName("main");
